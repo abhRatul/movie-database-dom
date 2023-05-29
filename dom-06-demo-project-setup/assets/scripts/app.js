9f5d3f897ebaaf36d9d8ab08628d7ea7733b79f7
abhRatul <ratula9@gmail.com>
2023-05-29 21:52:11 +0600

    select modal div and button with dom

---
+++ dom-06-demo-project-setup/assets/scripts/app.js
@@ -0,0 +1,8 @@
+const addMovieModal = document.querySelector('#add-modal');
+// const addMovieModal = document.getElementById('add-modal');
+// const addMovieModal = document.body.children[1];
+
+// const startAddMovieButton = document.querySelector('header button');
+const startAddMovieButton = document.querySelector('header').lastElementChild;
+
+console.log(startAddMovieButton);
