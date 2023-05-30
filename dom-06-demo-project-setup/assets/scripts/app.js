b5dfd4e3ffbdefed8240132e3f3062e18ef4b95e
abhRatul <ratula9@gmail.com>
2023-05-30 08:05:53 +0600

    Add backdrop Click Handler,cancelMovie Click Handler

--- dom-06-demo-project-setup/assets/scripts/app.js
+++ dom-06-demo-project-setup/assets/scripts/app.js
@@ -1,20 +1,29 @@
 const addMovieModal = document.querySelector('#add-modal');
 // const addMovieModal = document.getElementById('add-modal');
 // const addMovieModal = document.body.children[1];
-
 const startAddMovieButton = document.querySelector('header button');
 // const startAddMovieButton = document.querySelector('header').lastElementChild;
-
 const backdropElement = document.querySelector('#backdrop')
+const cancleAddMovieButton = addMovieModal.querySelector('.btn--passive');
 
 
 const toggleBackdrop = ()=>{
-    backdropElement.classList.toggle('visible')
+    backdropElement.classList.toggle('visible');
 }
 
 const toggleMovieModal = ()=> { // function(){}
     addMovieModal.classList.toggle('visible');
-    toggleBackdrop()
+    toggleBackdrop();
+}
+
+const backdropClickHandler = ()=> {
+    toggleMovieModal();
+}
+
+const cancleAddMovie = ()=> {
+    toggleMovieModal();
 }
 
 startAddMovieButton.addEventListener('click', toggleMovieModal)
+backdropElement.addEventListener('click', backdropClickHandler);
+cancleAddMovieButton.addEventListener('click',cancleAddMovie);
