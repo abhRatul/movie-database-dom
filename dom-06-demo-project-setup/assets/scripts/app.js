4da233bec0aa940494228796aac74b05a55653c7
abhRatul <ratula9@gmail.com>
2023-05-30 07:36:49 +0600

    add visible class to modal class

--- dom-06-demo-project-setup/assets/scripts/app.js
+++ dom-06-demo-project-setup/assets/scripts/app.js
@@ -2,7 +2,11 @@ const addMovieModal = document.querySelector('#add-modal');
 // const addMovieModal = document.getElementById('add-modal');
 // const addMovieModal = document.body.children[1];
 
-// const startAddMovieButton = document.querySelector('header button');
-const startAddMovieButton = document.querySelector('header').lastElementChild;
+const startAddMovieButton = document.querySelector('header button');
+// const startAddMovieButton = document.querySelector('header').lastElementChild;
 
-console.log(startAddMovieButton);
+const toggleMovieModal = () { // function(){}
+    addMovieModal.classList.toggle('visible');
+}
+
+startAddMovieButton.addEventListener('click', toggleMovieModal)
